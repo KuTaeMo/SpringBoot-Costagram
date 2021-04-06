1580c8a53641fbd0ce79f9b8dc4b84489007aabd
KuTaeMo <67215505+KuTaeMo@users.noreply.github.com>
2021-04-06 15:38:01 +0900

    20. 인스타그램 클론 코딩 - 회원프로필사진 등록 완료 인스타그램 끝

--- src/main/java/com/cos/costagram/service/UserService.java
+++ src/main/java/com/cos/costagram/service/UserService.java
@@ -2,13 +2,26 @@ package com.cos.costagram.service;
 
 
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.cos.costagram.config.auth.PrincipalDetails;
 import com.cos.costagram.domain.follow.FollowRepository;
+import com.cos.costagram.domain.image.Image;
+import com.cos.costagram.domain.tag.Tag;
 import com.cos.costagram.domain.user.User;
 import com.cos.costagram.domain.user.UserRepository;
+import com.cos.costagram.utils.TagUtils;
+import com.cos.costagram.web.dto.image.ImageReqDto;
 import com.cos.costagram.web.dto.user.UserProfileRespDto;
 
 import lombok.RequiredArgsConstructor;
@@ -20,6 +33,35 @@ public class UserService {
 	private final UserRepository userRepository;
 	private final FollowRepository followRepository;
 	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	
+	@Value("${file.path}")
+	private String uploadFolder;
+	
+	@Transactional
+	public User 회원사진변경(MultipartFile profileImageFile,PrincipalDetails principalDetails) {
+		
+		// 같은 이름의 이미지가 들어올 때 충돌을 방지하기 위해 uuid를 생성하고 이미지 이름 앞에 붙여줌
+		UUID uuid=UUID.randomUUID(); 
+		// 이미지 이름 뒤에 붙이면 확장자가 망가지기 때문에 앞에 붙인다.
+		// 앞에 날짜까지 붙이면 충돌날 확률 0에 가까움!
+		String imageFileName=uuid+"_"+profileImageFile.getOriginalFilename();
+		System.out.println("파일명 : "+imageFileName);
+		
+		Path imageFilePath=Paths.get(uploadFolder+imageFileName);
+		System.out.println("파일패스 : "+imageFilePath);
+		
+		try {
+			Files.write(imageFilePath, profileImageFile.getBytes());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		User userEntity=userRepository.findById(principalDetails.getUser().getId()).get();
+		userEntity.setProfileImageUrl(imageFileName);
+		
+		return userEntity;
+	}
 	
 	@Transactional(readOnly = true)
 	public UserProfileRespDto 회원프로필(int userId,int principalId) {
--- src/main/resources/static/js/image.js
+++ src/main/resources/static/js/image.js
@@ -1,7 +1,7 @@
 let page = 0;
 
-let principalId=$("#principal-Id").val();
-let username=$("#principal-username").val();
+let principalId = $("#principal-id").val();
+let username = $("#principal-username").val();
 
 function feedLoad() {
   // ajax로 Page<Image> 가져올 예정 (3개)
@@ -12,7 +12,7 @@ function feedLoad() {
   }).done((res) => {
     let images = res.data.content;
     images.forEach((image) => {
-      let feedBox = feedItem(image,principalId);
+      let feedBox = feedItem(image);
       $("#feed_list").append(feedBox);
     });
   });
@@ -22,102 +22,104 @@ feedLoad();
 
 $(window).scroll(() => {
   
-//   console.log("scrollTop", $(window).scrollTop());
-//   console.log("document height", $(document).height());
-//   console.log("window height", $(window).height());
-   
-   let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height());
-   
-   // 근사치 계산
-   if(checkNum < 1 && checkNum > -1){
-      page++;
-      feedLoad();
-   }
+//	console.log("scrollTop", $(window).scrollTop());
+//	console.log("document height", $(document).height());
+//	console.log("window height", $(window).height());
+	
+	let checkNum = $(window).scrollTop() - ($(document).height() - $(window).height());
+	
+	// 근사치 계산
+	if(checkNum < 1 && checkNum > -1){
+		page++;
+		feedLoad();
+	}
 });
 
 function feedItem(image) {
   let result = `
 <!--전체 리스트 아이템-->
 <div class="story-list__item">
-   <!--리스트 아이템 헤더영역-->
-   <div class="sl__item__header">
-      <div><img src="/upload/${image.user.profileImageUrl}" alt=""  onerror="this.src='/images/person.jpeg'"/><svg viewbox="0 0 110 110"><circle cx="55" cy="55" r="53" /></svg></div>
-      <div>${image.user.username}</div>
-   </div>
-   <!--헤더영역 end-->
-
-   <!--게시물이미지 영역-->
-   <div class="sl__item__img">
-      <img src="/upload/${image.postImageUrl}" alt="" />
-   </div>
-
-   <!--게시물 내용 + 댓글 영역-->
-   <div class="sl__item__contents">
-      <!-- 하트모양 버튼 박스 -->
-      <div class="sl__item__contents__icon"> `;
+	<!--리스트 아이템 헤더영역-->
+	<div class="sl__item__header">
+		<div><img src="/upload/${image.user.profileImageUrl}" alt=""  onerror="this.src='/images/person.jpeg'"/><svg viewbox="0 0 110 110"><circle cx="55" cy="55" r="53" /></svg></div>
+		<div>${image.user.username}</div>
+	</div>
+	<!--헤더영역 end-->
+	<!--게시물이미지 영역-->
+	<div class="sl__item__img">
+		<img src="/upload/${image.postImageUrl}" alt="" />
+	</div>
+	<!--게시물 내용 + 댓글 영역-->
+	<div class="sl__item__contents">
+		<!-- 하트모양 버튼 박스 -->
+		<div class="sl__item__contents__icon"> `;
 
   if (image.likeState) {
     result += `<button onclick="likeOrUnLike(${image.id})">
-                     <i class="fas fa-heart active" id="like_icon_${image.id}"></i>
-                  </button>`;
+							<i class="fas fa-heart active" id="like_icon_${image.id}"></i>
+						</button>`;
   } else {
     result += `<button onclick="likeOrUnLike(${image.id})">
-                     <i class="far fa-heart" id="like_icon_${image.id}"></i>
-                  </button>`;
+							<i class="far fa-heart" id="like_icon_${image.id}"></i>
+						</button>`;
   }
 
-  result += `   
-      </div>
-      <!-- 하트모양 버튼 박스 end -->
-
-      <!--좋아요-->
-      <span class="like"><b id="like_count_${image.id}">${image.likeCount}</b>likes</span>
-      <!--좋아요end-->
-
-      <!--태그박스-->
-      <div class="sl__item__contents__tags">
-         <p> `;
+  result += `	
+		</div>
+		<!-- 하트모양 버튼 박스 end -->
+		<!--좋아요-->
+		<span class="like"><b id="like_count_${image.id}">${image.likeCount}</b>likes</span>
+		<!--좋아요end-->
+		<!--태그박스-->
+		<div class="sl__item__contents__tags">
+			<p> `;
 
   image.tags.forEach((tag) => {
     result += `#${tag.name} `;
   });
 
-  result += `         
-         </p>
-      </div>
-      <!--태그박스end-->
-
-      <!--게시글내용-->
-      <div class="sl__item__contents__content">
-         <p>${image.caption}</p>
-      </div>
-      <!--게시글내용end-->
-      
-      <!-- 댓글 들어오는 박스 -->
-      <div id="comment-list-${image.id}">
-      `;
-
-	image.comments.forEach((comment)=>{
-		result+=`<div class="sl__item__contents__comment" id="comment-${comment.id}">
-       <p>
-         <b>${comment.user.username} :</b>
-         ${comment.content}
-       </p>
-       <button onClick="deleteComment(${comment.id})"><i class="fas fa-times"></i></button>
-     </div>`;
-	})
-	
-result += `
-      </div>
-      <!-- 댓글 들어오는 박스end -->
-
-      <!--댓글입력박스-->
-      <div class="sl__item__input">
-         <input type="text" placeholder="댓글 달기..." id="comment-${image.id}"/>
-         <button type="button" onClick="addComment(${image.id}, '${username}')">게시</button>
-      </div>
-      <!--댓글달기박스end-->
-   </div>
+  result += `			
+			</p>
+		</div>
+		<!--태그박스end-->
+		<!--게시글내용-->
+		<div class="sl__item__contents__content">
+			<p>${image.caption}</p>
+		</div>
+		<!--게시글내용end-->
+		
+		<!-- 댓글 들어오는 박스 -->
+		<div id="comment-list-${image.id}">
+		`;
+  
+  		image.comments.forEach((comment)=>{
+  			result += `	<div class="sl__item__contents__comment" id="comment-${comment.id}"">
+			    <p>
+			      <b>${comment.user.username} :</b>
+			      ${comment.content}
+			    </p>
+  				`;
+  			
+  			if(principalId == comment.user.id){
+  	  			result +=`
+  				    <button onClick="deleteComment(${comment.id})"><i class="fas fa-times"></i></button>
+  				`;
+  			}
+  			
+  			result += `
+			  </div>`;
+  		});
+  
+  		result += `
+		</div>
+		<!-- 댓글 들어오는 박스end -->
+		<!--댓글입력박스-->
+		<div class="sl__item__input">
+			<input type="text" placeholder="댓글 달기..." id="comment-${image.id}"/>
+			<button type="button" onClick="addComment(${image.id}, '${username}')">게시</button>
+		</div>
+		<!--댓글달기박스end-->
+	</div>
 </div>
 <!--전체 리스트 아이템end-->
 `;
@@ -126,63 +128,50 @@ result += `
 
 
 function addComment(imageId, username) {
-   
-   console.log("imageId", imageId);
-   console.log("username", username);
-   
-     let commentInput = $("#comment-"+imageId);
-     let commentList = $("#comment-list-"+imageId);
+	
+	  let commentInput = $("#comment-"+imageId);
+	  let commentList = $("#comment-list-"+imageId);
 
-     // 유저 아이디 필요하면 매개변수로 받아와서 넣으면 됨.
-     let _data = {
-       imageId: imageId,
-       commemtText: commentInput.val(),
-     };
-     if (_data.commemtText === "" || _data.commemtText === null) {
-       alert("댓글을 작성해주세요!");
-       return;
-     }
-     
-     // 통신 성공하면 아래 prepend 되야 되고 ID값 필요함
-	$.ajax({
-		type:"post",
-		url:`/image/${imageId}/comment`,
-		data:_data.commemtText,
-		contentType:"plain/text; charset=utf-8",
-		dataType:"json"
-	}).done((res)=>{
-		console.log(res.data);
-		let content = `
-     <div class="sl__item__contents__comment" id="comment-${comment.id}">
-       <p>
-         <b>${username} :</b>
-         ${_data.commemtText}
-       </p>
-		`;
-		
-		// 댓글 자신이 쓴거만 삭제하게
-		if(principalId==comment.id){
-			result+=`
-			<button onClick="deleteComment(${comment.id})"><i class="fas fa-times"></i></button>
-			`;
-		}
-       
-	result+=`
-     </div>
-     `;
-     commentList.prepend(content);
-     commentInput.val("");
-	});
+	  // 유저 아이디 필요하면 매개변수로 받아와서 넣으면 됨.
+	  let _data = {
+	    imageId: imageId,
+	    commemtText: commentInput.val(),
+	  };
+	  if (_data.commemtText === "") {
+	    alert("댓글을 작성해주세요!");
+	    return;
+	  }
 
-     
-   }
+	  // 통신 성공하면 아래 prepend 되야 되고 ID값 필요함
+	  $.ajax({
+		  type: "POST",
+		  url: `/image/${imageId}/comment`,
+		  data: _data.commemtText,
+		  contentType: "plain/text; charset=utf-8",
+		  dataType: "json"
+	  }).done(res=>{
+		  let comment = res.data;
+		  let content = `
+			  <div class="sl__item__contents__comment" id="comment-${comment.id}">
+			    <p>
+			      <b>${username} :</b>
+			      ${_data.commemtText}
+			    </p>
+			    <button onClick="deleteComment(${comment.id})"><i class="fas fa-times"></i></button>
+			  </div>
+			  `;
+			  commentList.prepend(content);
+			  commentInput.val("");
+	  });
+	  
+}
 
-   function deleteComment(commentId) {
-     $.ajax({
-		type:"delete",
-		url:"/comment/"+commentId,
-		dataType:"json"
-}).done((res)=>{
-	$("#comment-"+commentId).remove();
-});
-   }
+function deleteComment(commentId) {
+  $.ajax({
+	  type: "delete",
+	  url: "/comment/"+commentId,
+	  dataType: "json"
+  }).done(res=>{
+	  $("#comment-"+commentId).remove();
+  });
+}
